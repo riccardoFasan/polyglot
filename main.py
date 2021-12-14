fa14c7b408b656347ffc4f891ea1ca546e9af42f
Riccardo Fasan <fasanriccardo21@gmail.com>
2021-12-14 12:18:59 +0100

    added progress bar

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import requests, json, datetime
+import requests, json, datetime, progressbar
 
 KEY = 'fc589597-ab6f-4955-b98f-f6ba8fa80246:fx'
 URL = 'https://api-free.deepl.com/v2/'
@@ -32,19 +32,34 @@ def api_call(word):
     print(f'No traslation found\n')
     return ''
 
-def translate(dict):
+def translate(dict, bar, i=0):
     for key, value in dict.items():
         if isinstance(value, __builtins__.dict):
-            translate(value)
+            translate(value, bar, i)
         else:
             dict[key] = api_call(value)
+            i += 1
+            bar.update(i)
+
+def get_number_of_dicts(dict):
+    number = 0
+    for key, value in dict.items():
+        if isinstance(value, __builtins__.dict):
+            number += get_number_of_dicts(value)
+        else:
+            number += 1
+    return number
 
 START = datetime.datetime.now()
 print(f'Translator is running.\nScript started at {START}.')
 
 get_usage_info()
 data = json.load(SOURCE)
-translate(data)
+
+NUMBER_OF_DICTS = get_number_of_dicts(data)
+
+with progressbar.ProgressBar(max_value=NUMBER_OF_DICTS, redirect_stdout=True) as bar:
+    translate(data, bar)
 
 try:
     DESTINATION = open(f'./{LANG.lower()}.json', 'x')
