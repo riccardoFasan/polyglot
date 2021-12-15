853899bf994d8ccc69d1e94496de7986e09aa918
Riccardo Fasan <fasanriccardo21@gmail.com>
2021-12-15 19:02:24 +0100

    POManager methods done and refactoring

--- translation.py
+++ translation.py
@@ -1,4 +1,4 @@
-import os, json, progressbar
+import os, json, progressbar, polib
 from colorama import Fore
 
 class AbstractManager: 
@@ -7,6 +7,8 @@ class AbstractManager:
 
     completion_count = 0
     progress_bar = None
+
+    translations_dict = dict()
 
     def __init__(self, deepl, source_file, target_path=None):
         self.deepl = deepl
@@ -18,47 +20,20 @@ class AbstractManager:
     def target_file(self):
         return f'{self.target_path}/{self.deepl.target_lang.lower()}.{self.EXTENSION}'
 
+    def get_progress_bar(self):
+        number_of_translations = self.number_of_translations()
+        return progressbar.ProgressBar(max_value=number_of_translations, redirect_stdout=True)
+
     def check_source_file(self):
         if not os.path.exists(self.source_file):
             print(Fore.RED + f'Error: "{self.source_file}" does not exist!')
             os._exit(0)
 
     def translate_source_file(self):
-        self.load_source()
+        self.load_translations_dict()
         self.progress_bar = self.get_progress_bar()
-
-    def load_source(self):
-        pass
-
-    def number_of_translations(self):
-        pass
-
-    def get_progress_bar(self):
-        number_of_translations = self.number_of_translations()
-        return progressbar.ProgressBar(max_value=number_of_translations, redirect_stdout=True)
-
-class JSONManager(AbstractManager):
-
-    EXTENSION = 'json'
-
-    translations_dict = dict()
-
-    def load_source(self):
-        with open(self.source_file, 'r') as source:
-            self.translations_dict = json.load(source) 
-
-    def translate_source_file(self):
-        super().translate_source_file()
         self.translate_dict()
         self.write_translations()
-
-    def number_of_translations(self, obj=None):
-        if obj == None:     
-            obj = self.translations_dict
-        number = 0
-        for key, value in obj.items():
-            number += self.number_of_translations(value) if isinstance(value, dict) else  1
-        return number
 
     def translate_dict(self, obj=None):
         if obj == None: 
@@ -71,6 +46,28 @@ class JSONManager(AbstractManager):
                 self.completion_count += 1
                 self.progress_bar.update(self.completion_count)
 
+    def number_of_translations(self, obj=None):
+        if obj == None:     
+            obj = self.translations_dict
+        number = 0
+        for key, value in obj.items():
+            number += self.number_of_translations(value) if isinstance(value, dict) else  1
+        return number
+
+    def load_translations_dict(self):
+        pass
+
+    def write_translations(self):
+        pass
+
+class JSONManager(AbstractManager):
+
+    EXTENSION = 'json'
+
+    def load_translations_dict(self):
+        with open(self.source_file, 'r') as source:
+            self.translations_dict = json.load(source) 
+
     def write_translations(self):
         with open(self.target_file, 'a+') as destination:
             destination.write(json.dumps(self.translations_dict, indent=2))
@@ -79,9 +76,13 @@ class PoManager(AbstractManager):
 
     EXTENSION = 'po'
 
-    def load_source(self):
-        with open(self.source_file, 'r') as source:
-            print(source.read())
+    def load_translations_dict(self):
+        pofile = polib.pofile(self.source_file)
+        for entry in pofile:
+            self.translations_dict[entry.msgid] = entry.msgstr
 
-    def translate_source_file(self):
-        super().translate_source_file()
+    def write_translations(self):
+        with open(self.target_file, 'a+') as destination:
+            for key, value in self.translations_dict.items():
+                destination.write(f'\nmsgid "{key}"\n')
+                destination.write(f'msgstr "{value}"\n')
