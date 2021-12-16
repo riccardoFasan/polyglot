b7301545e320dce1e1370bcc83ba5fc201f6ee99
Riccardo Fasan <57989127+riccardoFasan@users.noreply.github.com>
2021-12-16 14:42:19 +0100

    argparse help updated

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ parser = argparse.ArgumentParser(description='Using the DeepL API, this script t
 parser.add_argument('action', type=str, help="The action that will be exectued.", choices=ACTIONS)
 parser.add_argument('-p', '--source_file', type=str, help='The JSON or PO file to be translated. Required if the action is "translate"', default=None)
 parser.add_argument('-t', '--target_lang', type=str, help='the code of the language into which you want to translate the source file. Required if the action is "translate"', default=None)
-parser.add_argument('-s', '--source_lang', type=str, help='Source file language code. Detected automatically by DeepL by default. Specifying it manually can increase performance and make translations more accurate.', default=None)
-parser.add_argument('-o', '--target_directory', type=str, help='The directory where the output file will be located. The working directory will be used if the passed directory is empty or not valid.', default=None)
+parser.add_argument('-o', '--target_directory', type=str, help='The directory where the output file will be located. Will be used the working directory if this option is invalid or not used.', default=None)
+parser.add_argument('-s', '--source_lang', type=str, help='Source file language code. Detected automatically by DeepL by default. Specifying it can increase performance and make translations more accurate.', default=None)
 
 args = parser.parse_args()
 
