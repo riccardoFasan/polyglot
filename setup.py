18ee4e6e08afd8d33bdcdf969e296f6853396ab5
Riccardo Fasan <57989127+riccardoFasan@users.noreply.github.com>
2021-12-18 16:01:13 +0100

    version updated

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="deeply",
-    version="0.0.4",
+    version="0.0.5",
     description="Automation CLI tool that, using the DeepL API, generates a JSON or a PO file from a given source file.",
     long_description=README,
     long_description_content_type="text/markdown",
