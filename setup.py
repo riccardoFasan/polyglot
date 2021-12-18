e363b23a7f2c8eeef4b9fd0d1333810b3cbbc5cf
Riccardo Fasan <57989127+riccardoFasan@users.noreply.github.com>
2021-12-18 16:00:44 +0100

    config fix

--- setup.py
+++ setup.py
@@ -28,9 +28,9 @@ setup(
     packages=find_packages(exclude=("tests",)),
     include_package_data=True,
     install_requires=[
-        "colorama"
-        "polib"
-        "progressbar2"
+        "colorama",
+        "polib",
+        "progressbar2",
         "requests"
     ],
     entry_points={
