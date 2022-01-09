1e5acd5a8e0e3583ba0a5ced2aa01f233392ec10
Riccardo Fasan <57989127+riccardoFasan@users.noreply.github.com>
2022-01-09 16:51:20 +0100

    feat: auto base_url detection depending on license version

--- polyglot/__init__.py
+++ polyglot/__init__.py
@@ -1,2 +1,2 @@
 #!/usr/bin/env python3
-__version__ = "1.4.4"
+__version__ = "1.5.4"
--- polyglot/deepl_request.py
+++ polyglot/deepl_request.py
@@ -1,6 +1,6 @@
 import requests
 import json
-from io import TextIOWrapper
+import os
 from colorama import Fore
 from pathlib import Path
 from requests.models import Response
@@ -8,60 +8,88 @@ from requests.models import Response
 
 class DeeplRequest:
 
-    BASE_URL: str = 'https://api-free.deepl.com/v2/'
     LEN_LIMIT: int = 150
+    license: dict = {
+        'version': 'free',  # 'free', 'pro' or 'invalid'
+        'key': ''
+    }
 
     def __init__(self, target_lang: str = None, source_lang: str = None):
         self.target_lang = target_lang
         self.source_lang = source_lang
-        self.key = self.get_or_ask_key()
+        self.apply_license()
 
     @property
-    def key_path(self):
-        return f'{Path.home()}/.deepl_api_key.txt'
+    def base_url(self):
+        version: str = '' if self.license['version'] == 'pro' else '-free'
+        return f'https://api{version}.deepl.com/v2/'
+
+    @property
+    def license_path(self):
+        return f'{Path.home()}/.deepl_api_key.json'
 
     @property
     def headers(self):
         return {
-            'Authorization': f'DeepL-Auth-Key {self.key}',
+            'Authorization': f'DeepL-Auth-Key {self.license["key"]}',
             'Content-Type': 'application/json'
         }
 
-    def get_or_ask_key(self):
-        with open(self.key_path, 'a+') as key_file:
-            key_file.seek(0)  #  Nosense, but OK
-            file_content: str = key_file.read()
-            key: str = self.ask_and_save_key(
-                key_file) if file_content == '' else file_content
-            return key
+    def apply_license(self):
+        try:
+            with open(self.license_path, 'r') as license_file:
+                file_content: dict = json.load(license_file)
+
+                if file_content['key'] and file_content['version']:
+                    self.license['key'] = file_content['key']
+                    self.license['version'] = file_content['version']
+
+        except:
+            self.set_key()
 
     def set_key(self):
-        with open(self.key_path, 'w+') as key_file:
-            self.ask_and_save_key(key_file)
+        with open(self.license_path, 'w+') as license_file:
+            self.license['key'] = input('Type here your Deepl API key: ')
+            self.license['version'] = self.get_key_version()
+            self.verify_license()
+            license: dict = {
+                'key': self.license['key'],
+                'version': self.license['version']
+            }
+            license_file.write(json.dumps(license, indent=2))
 
-    def ask_and_save_key(self, key_file: TextIOWrapper):
-        key = input('Type here your Deepl API key: ')
-        key_file.write(key)
-        return key
+    def get_key_version(self):
+        response: Response = self.get_usage_info()
+        if response.status_code == 403:
+            if self.license['version'] == 'free':
+                self.license['version'] = 'pro'
+                self.get_key_version()
+            return 'invalid'
+        return self.license['version']
+
+    def verify_license(self):
+        if self.license['version'] == 'invalid':
+            print(f'{Fore.RED}\nThis key is invalid.\n')
+            os._exit(0)
+
+    def get_usage_info(self):
+        return requests.get(f'{self.base_url}usage', headers=self.headers)
 
     def print_usage_info(self):
-        response: Response = requests.get(
-            f'{self.BASE_URL}usage', headers=self.headers)
+        response: Response = self.get_usage_info()
 
         if response.status_code == 200:
             body: dict = json.loads(response.text)
             character_count: int = body['character_count']
             character_limit: int = body['character_limit']
             percentage: int = round((character_count / character_limit) * 100)
-            print(
-                f"\nAPI key: {self.key}.\nCharacters limit: {character_limit}")
             print_color: str = self.get_color_by_percentage(percentage)
             print(
-                f"{print_color}Used characters: {character_count} ({percentage}%)\n")
+                f"\nAPI key: {self.license['key']}.\nCharacters limit: {character_limit}\n{print_color}Used characters: {character_count} ({percentage}%)\n")
 
         else:
-            print(f"{Fore.RED}\nError retrieving usage info.",
-                  f'Error code: {response.status_code}.\n')
+            print(
+                f"{Fore.RED}\nError retrieving usage info.\nError code: {response.status_code}.\n")
 
     def get_color_by_percentage(self, percentage: int):
         if percentage > 90:
@@ -72,7 +100,7 @@ class DeeplRequest:
 
     def print_supported_languages(self):
         response: Response = requests.get(
-            f'{self.BASE_URL}languages', headers=self.headers)
+            f'{self.base_url}languages', headers=self.headers)
 
         if response.status_code == 200:
             body: dict = json.loads(response.text)
@@ -85,7 +113,7 @@ class DeeplRequest:
                 f'{Fore.RED}\nError retrieving the supported languages.\nError code: {response.status_code}\n')
 
     def translate(self, entry: str):
-        endpoint: str = f"{self.BASE_URL}translate?auth_key={self.key}&text={entry}&target_lang={self.target_lang}"
+        endpoint: str = f"{self.base_url}translate?auth_key={self.license['key']}&text={entry}&target_lang={self.target_lang}"
 
         if self.source_lang:
             endpoint += f"&source_lang={self.source_lang}"
