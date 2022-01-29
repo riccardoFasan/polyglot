83650214271bb1b011e0765a36da699eb11f8620
Riccardo Fasan <57989127+riccardoFasan@users.noreply.github.com>
2022-01-29 23:35:46 +0100

    refactor: improved typing

--- polyglot/managers.py
+++ polyglot/managers.py
@@ -11,7 +11,7 @@ from polyglot import deepl_request
 
 class Manager(ABC):
 
-    def __init__(self, deepl: deepl_request.DeeplRequest, source_file: str, output_directory: str = ''):
+    def __init__(self, deepl: deepl_request.DeeplRequest, source_file: str, output_directory: str = '') -> None:
         self.source_file = source_file
         self.check_source_file()
         self.deepl = deepl
@@ -19,21 +19,21 @@ class Manager(ABC):
             output_directory) else os.getcwd()
 
     @property
-    def extension(self):
+    def extension(self) -> str:
         name, extension = os.path.splitext(self.source_file)
         return extension
 
     @property
-    def target_file(self):
+    def target_file(self) -> str:
         return f'{self.output_directory}/{self.deepl.target_lang.lower()}{self.extension}'
 
-    def check_source_file(self):
+    def check_source_file(self) -> None:
         if not os.path.exists(self.source_file):
             print(f'{colorama.Fore.RED}Error: "{self.source_file}" does not exist!')
             os._exit(0)
 
     @abstractmethod
-    def translate_source_file(self):
+    def translate_source_file(self) -> None:
         pass
 
 
@@ -41,13 +41,13 @@ class TextManager(Manager):
 
     content: str = ''
 
-    def translate_source_file(self):
+    def translate_source_file(self) -> None:
         self.load_source_content()
         self.translate_content()
         if self.content:
             self.make_translated_files()
 
-    def load_source_content(self):
+    def load_source_content(self) -> None:
         try:
             with open(self.source_file, 'r') as source:
                 self.content = source.read()
@@ -55,10 +55,10 @@ class TextManager(Manager):
             print(f'{colorama.Fore.RED}Cannot read {self.extension} files.')
             os._exit(0)
 
-    def translate_content(self):
+    def translate_content(self) -> None:
         self.content = self.deepl.translate(self.content)
 
-    def make_translated_files(self):
+    def make_translated_files(self) -> None:
         with open(self.target_file, 'w+') as destination:
             destination.write(self.content)
             print(f'Generated {self.target_file}.')
@@ -71,27 +71,27 @@ class DictionaryManager(TextManager):
     progress_bar: progressbar.ProgressBar
     content: dict = dict()
 
-    def translate_source_file(self):
+    def translate_source_file(self) -> None:
         self.load_source_content()
         self.progress_bar = self.get_progress_bar()
         self.translate_content()
         self.print_ending_messages()
         self.make_translated_files()
 
-    def get_progress_bar(self):
+    def get_progress_bar(self) -> progressbar.ProgressBar:
         number_of_translations: int = self.get_number_of_translations()
         return progressbar.ProgressBar(max_value=number_of_translations, redirect_stdout=True)
 
     def get_number_of_translations(self) -> int:
         return 0
 
-    def print_ending_messages(self):
+    def print_ending_messages(self) -> None:
         print('\nTranslation completed.')
         if self.not_translated_count > 0:
             print(
                 f'{colorama.Fore.YELLOW}\n{self.not_translated_count} entries have not been translated.\n')
 
-    def translate_and_handle(self, entry: str):
+    def translate_and_handle(self, entry: str) -> str:
         translation: str | None = self.deepl.translate(entry)
         if not translation:
             self.not_translated_count += 1
@@ -100,7 +100,7 @@ class DictionaryManager(TextManager):
 
 class JSONManager(DictionaryManager):
 
-    def get_number_of_translations(self, dictionary: dict[str, str] | None = None):
+    def get_number_of_translations(self, dictionary: dict[str, str] | None = None) -> int:
         if not dictionary:
             dictionary = self.content
         number: int = 0
@@ -111,11 +111,11 @@ class JSONManager(DictionaryManager):
 
         return number
 
-    def load_source_content(self):
+    def load_source_content(self) -> None:
         with open(self.source_file, 'r') as source:
             self.content = json.load(source)
 
-    def translate_content(self, dictionary: dict[str, str] | None = None):
+    def translate_content(self, dictionary: dict[str, str] | None = None) -> None:
 
         if not dictionary:
             dictionary = self.content
@@ -130,7 +130,7 @@ class JSONManager(DictionaryManager):
                 self.completion_count += 1
                 self.progress_bar.update(self.completion_count)
 
-    def make_translated_files(self):
+    def make_translated_files(self) -> None:
         with open(self.target_file, 'w+') as destination:
             destination.write(json.dumps(self.content, indent=2))
             print(f'Generated {self.target_file}.')
@@ -139,13 +139,13 @@ class JSONManager(DictionaryManager):
 class POManager(DictionaryManager):
 
     @property
-    def pofile_source(self):
+    def pofile_source(self) -> polib.POFile:
         return polib.pofile(self.source_file)
 
-    def get_number_of_translations(self):
+    def get_number_of_translations(self) -> int:
         return len(self.content.items())
 
-    def load_source_content(self):
+    def load_source_content(self) -> None:
         pofile: polib.POFile = self.pofile_source
 
         for entry in pofile:
@@ -154,13 +154,13 @@ class POManager(DictionaryManager):
                 "occurrences": entry.occurrences
             }
 
-    def translate_content(self):
+    def translate_content(self) -> None:
         for key, value in self.content.items():
             value['msgstr'] = self.translate_and_handle(value['msgstr'])
             self.completion_count += 1
             self.progress_bar.update(self.completion_count)
 
-    def make_translated_files(self):
+    def make_translated_files(self) -> None:
         pofile: polib.POFile = polib.POFile()
         pofile.metadata = self.pofile_source.metadata
 
@@ -184,14 +184,14 @@ class DocumentManager(Manager):
     document_id: str = ''
     document_key: str = ''
 
-    def translate_source_file(self):
+    def translate_source_file(self) -> None:
         document_data: dict[str, str] = self.deepl.translate_document(
             self.source_file)
         self.document_id = document_data['document_id']
         self.document_key = document_data['document_key']
         self.download_document_when_ready()
 
-    def download_document_when_ready(self):
+    def download_document_when_ready(self) -> None:
         status_data = self.deepl.check_document_status(
             self.document_id, self.document_key)
         status: str = status_data['status']
@@ -209,7 +209,7 @@ class DocumentManager(Manager):
 
         self.download_document_when_ready()
 
-    def download_target_file(self):
+    def download_target_file(self) -> None:
         binaries: bytes = self.deepl.download_translated_document(
             self.document_id, self.document_key)
         with open(self.target_file, 'wb+') as destination:
