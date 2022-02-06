fd28f3644aa9e2881a757f6b46d90502e3f22eab
Riccardo Fasan <57989127+riccardoFasan@users.noreply.github.com>
2022-02-06 15:00:01 +0100

    refactor: license version replace with enum

--- polyglot/license.py
+++ polyglot/license.py
@@ -1,24 +1,32 @@
-import json, pathlib
+from enum import Enum
+import json
+import pathlib
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from distutils.util import strtobool
 
 
+class LicenseVersion(Enum):
+    FREE = 'free'
+    PRO = 'pro'
+
+
 @dataclass
 class License:
     key: str = ''
-    version: str = 'free'  # 'free' or 'pro'
+    version: LicenseVersion = LicenseVersion.FREE
 
 
 class LicenseManager(ABC):
 
     @abstractmethod
     def get_license(self) -> License:
-        return License(key='', version='')
+        return License(key='', version=LicenseVersion.FREE)
 
     @abstractmethod
     def set_license(self) -> None:
         pass
+
 
 class CLILicenseManager(LicenseManager):
 
@@ -32,7 +40,7 @@ class CLILicenseManager(LicenseManager):
                 file_content: dict[str, str] = json.load(license_file)
 
                 if file_content['key'] and file_content['version']:
-                    return License(key=file_content['key'], version=file_content['version'])
+                    return License(key=file_content['key'], version=LicenseVersion(file_content['version']))
                 return self.__set_and_get_license()
         except:
             return self.__set_and_get_license()
@@ -44,11 +52,11 @@ class CLILicenseManager(LicenseManager):
     def set_license(self) -> None:
         with open(self.__license_path, 'w+') as license_file:
             key: str = input('Type here your Deepl API key: ')
-            version: str = 'pro' if self.__yes_no_input(
-                'Are you using the pro license?') else 'free'
+            version: LicenseVersion = LicenseVersion.PRO if self.__yes_no_input(
+                'Are you using the pro license?') else  LicenseVersion.FREE
             license: dict[str, str] = {
                 'key': key,
-                'version': version
+                'version': version.value
             }
             license_file.write(json.dumps(license, indent=2))
 
